ede80e3e05a3214625833cf6f5be078f1667297c
uma <hsinyi.wc.uma@gmail.com>
2019-05-18 18:02:42 -0700

    Implement API calls for searchMovie

--- searchMovieDB.js
+++ searchMovieDB.js
@@ -1,28 +1,26 @@
 import API_KEY from "./config/config.js"
 
-function searchMovieDB(q){
+function searchMovieDB(q) {
     var option = {
         method: 'GET'
     }
     console.log(q)
-    var qstr = 'https://api.themoviedb.org/3/search/movie?api_key='+ API_KEY+ '&query=' + q.split(' ').join('+');
+    var qstr = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query=' + q.split(' ').join('+');
     console.log(qstr);
     fetch(qstr, option)
-    .then(function (response) {
-        var res = response.json(); 
-        return res;
-    })
-    .then(i=>{
-        // var res = JSON.stringify(i);
-        return i.results;
-    })
-    .then(res=>{
-        console.log(res);
-    })
-    .catch(e => { throw e });
+        .then(function(response) {
+            var res = response.json();
+            return res;
+        })
+        .then(i => {
+            // var res = JSON.stringify(i);
+            return i.results;
+        })
+        .then(res => {
+            console.log(res);
+        })
+        .catch(e => { throw e });
 }
 
 
 export default searchMovieDB;
-
-
